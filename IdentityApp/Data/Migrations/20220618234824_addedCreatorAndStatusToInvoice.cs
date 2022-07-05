af8747cb2bef8be161a152a0cfbb33d1298fce14
sequester0 <i.keremgumus@hotmail.com>
2022-07-06 00:43:41 +0300

    initial commit

---
+++ IdentityApp/Data/Migrations/20220618234824_addedCreatorAndStatusToInvoice.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityApp.Data.Migrations
+{
+    public partial class addedCreatorAndStatusToInvoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatorId",
+                table: "Invoice",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Invoice",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Invoice");
+        }
+    }
+}
---
+++ IdentityApp/Pages/Invoices/Details.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using IdentityApp.Data;
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using IdentityApp.Authorization;
+
+namespace IdentityApp.Pages.Invoices
+{
+    public class DetailsModel : DI_BasePageModel
+    {
+        public DetailsModel(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
+        {
+        }
+
+      public Invoice Invoice { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || Context.Invoice == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await Context.Invoice.FirstOrDefaultAsync(m => m.InvoiceId == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Invoice = invoice;
+            }
+
+            var isCreator = await AuthorizationService.AuthorizeAsync(User, Invoice, InvoiceOperations.Read);
+
+            var isManager = User.IsInRole(Constants.InvoiceManagersRole);
+
+            if (isCreator.Succeeded == false && isManager == false) { return Forbid(); }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id, InvoiceStatus status)
+        {
+            Invoice = await Context.Invoice.FindAsync(id);
+            
+            if (Invoice == null)
+            {
+                return NotFound();
+            }
+
+            var invoiceOperation = status == InvoiceStatus.Approved ? InvoiceOperations.Approve : InvoiceOperations.Reject;
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(User, Invoice, invoiceOperation);
+
+            if (isAuthorized.Succeeded == false)
+            {
+                return Forbid();
+            }
+
+            Invoice.Status = status;
+            Context.Invoice.Update(Invoice);
+
+            await Context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
